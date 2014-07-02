e6e22959d38595fd20532d0a2d5bd70e3fe7685b
German Ostaszynski <german92@gmail.com>
2014-07-02 23:57:41 +0200

    deleted and recreated user_controllers.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,4 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!
-
-
-	# def index 
-	# 	@users = User.all 
-	# end
-
-
-	def show
-		# if user_signed_in?
-		# 	...
-		# else 
-		# 	redirect_to '/home'
-	end
-
-
-	# end
-
-	# private
-
-	# def user_params
-	# 	params.require(:user).permit(:name)
-	# end
-
-
-
+  def show
+  end
 end
-
-# Emmet table	
-# ERB Snippets  
-# HTML Beautify - tabs everything
-# ColorPicker - picks colors easily
