7cec54a0d58b55888dcd86b3fe610f44c48d02e5
German Ostaszynski <german92@gmail.com>
2014-07-03 22:27:24 +0200

    Fixed apartments show and index (listings)

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -11,22 +11,31 @@ class ApartmentsController < ApplicationController
 
 	def create
 		@apartment = Apartment.new(apartment_params)
-
 		@apartment.user_id = current_user.id
 
-
-		respond_to do |format|	
-			if @apartment.save
-				format.html { redirect_to @apartment, notice: 'Apartment was successfully created.' }
-				format.json { render :show, status: :created, location: @apartment }
-			else
-				format.html { render :new }
-				format.json { render json: @task.errors, status: :unprocessable_entity }
-			end
-		end
-
+		if @apartment.save
+			redirect_to apartments_path
+		else
+      		render :new
+    	end
 	end
 
+	# def edit
+	# end
+
+	def update
+		# @apartment = Apartment.edit
+
+		respond_to do |format|
+	  		if @apartment.update(apartment_params)
+	    		format.html { redirect_to @apartment, notice: 'Apartment was successfully updated.' }
+	    		format.json { render :show, status: :ok, location: @apartment }
+	  		else
+	    		format.html { render :edit }
+	    		format.json { render json: @apartment.errors, status: :unprocessable_entity }
+	  		end
+		end
+	end
 
 
 	def show
