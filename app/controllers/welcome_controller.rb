a4eb4669c88ea598adf7a18649c9815b1ab67a3b
German Ostaszynski <german92@gmail.com>
2014-08-18 15:33:13 +0200

    Fix welcome files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,16 +3,15 @@ class WelcomeController < ApplicationController
   end
 
   def home
-  	if user_signed_in? 
-  		redirect_to "/users/#{current_user.id}"
-  	end 
-
+    if user_signed_in? 
+      redirect_to "/users/#{current_user.id}"
+    end 
   end
 
   def contact
     render 'contact'
   end
-  
+
   def about
     render 'about'
   end
